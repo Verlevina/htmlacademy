5e6ae9f742186c16f7be788e9bcbd661aeb8e014
Verlevina <levitinava@gmail.com>
2018-04-24 11:40:58 +0300

    переключение актив и мобильная навигация без нормального скрытия

--- www/js/main.js
+++ www/js/main.js
@@ -4,7 +4,14 @@ function controls(){
     var controlEl=document.getElementById('navButton');
     controlEl.addEventListener('click',showNav);
 }
-function showNav(){
+function showNav(event){
     var nav=document.getElementsByTagName('nav')[0];
-    nav.className += 'showNav';
+    nav.classList.toggle('showNav');
+
+    nav.addEventListener('click',activeClick);
+
+}
+function activeClick(event){
+    document.getElementsByClassName('active')[0].classList.remove('active');
+    event.target.classList.add('active');
 }
