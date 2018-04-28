3c53fb17831be44ecf6c28e281afcc97dca39c63
Verlevina <levitinava@gmail.com>
2018-04-28 09:34:40 +0300

    добавлен скролл

--- www/js/main.js
+++ www/js/main.js
@@ -15,3 +15,14 @@ function activeClick(event){
     document.getElementsByClassName('active')[0].classList.remove('active');
     event.target.classList.add('active');
 }
+
+
+//скролл для навигации
+jQuery(document).ready(function() {
+    jQuery(".scrollto a").click(function () {
+        elementClick = jQuery(this).attr("href")
+        destination = jQuery(elementClick).offset().top - 100;
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+        return false;
+    });
+});
