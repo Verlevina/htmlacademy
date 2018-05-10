e1d2f29fdfac5f0746c04e15da6dd6c4dcae59b6
Verlevina <levitinava@gmail.com>
2018-05-10 21:26:53 +0300

    commit

--- www/js/main.js
+++ www/js/main.js
@@ -6,8 +6,9 @@ function controls(){
     controlEl.addEventListener('click',showNav);
 }
 function showNav(event){
-    var nav=document.getElementsByTagName('nav')[0];
+    var nav=$('nav')[0];
     nav.classList.toggle('showNav');
+    nav.show( "slow" );
     for (var i=0;i<a.length;i++ ) {
         a[i].addEventListener('click', activeClick);
     }
