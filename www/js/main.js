4bb88873c5baaba5904ecbd900228e697716d0cf
Verlevina <levitinava@gmail.com>
2018-05-17 10:30:09 +0300

    сделано выезжающее меню

--- www/js/main.js
+++ www/js/main.js
@@ -1,18 +1,11 @@
 // управление меню в мобильной версии
-controls();
+
 var a=document.getElementsByTagName('a');
-function controls(){
-    var controlEl=document.getElementById('navButton');
-    controlEl.addEventListener('click',showNav);
+for (var i=0;i<a.length;i++ ) {
+    a[i].addEventListener('click', activeClick);
 }
-function showNav(event){
-    var nav=$('nav');
-    nav.toggleClass('showNav');
-    nav.slideToggle('slow');
-    for (var i=0;i<a.length;i++ ) {
-        a[i].addEventListener('click', activeClick);
-    }
-}
+
+
 function activeClick(event){
     document.getElementsByClassName('active')[0].classList.remove('active');
     this.classList.add('active');
@@ -24,9 +17,67 @@ for (var i=0;i<a.length;i++ ) {
 //скролл для навигации
 jQuery(document).ready(function() {
     jQuery(".scrollto a").click(function () {
-        elementClick = jQuery(this).attr("href")
+        elementClick = jQuery(this).attr("href");
         destination = jQuery(elementClick).offset().top - 0;
         jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
         return false;
     });
 });
+
+
+var main = function() { //главная функция
+    var nav=$('nav');
+    var body=$('body');
+    var navRight=0;
+    var bodyRight=135;
+    var navRightBegin=-135;
+    var bodyRightBegin=0;
+    nav.css('right',navRightBegin);
+    body.css('right',bodyRightBegin);
+    $('.navButton').on('click', function() {
+        if (nav.css('right') == navRightBegin+'px') {
+
+            nav.animate({
+               right:navRight
+            }, 200);
+
+            body.animate({
+               right:bodyRight
+            }, 200);
+        }
+        if (nav.css('right') == navRight+'px') {
+            nav.animate({
+               right:navRightBegin
+            }, 200);
+
+            body.animate({
+               right:bodyRightBegin
+            }, 200);
+        }
+    });
+};
+
+
+    /* Закрытие меню */
+//
+//     $('.icon-close').click(function() { //выбираем класс icon-close и метод click
+//
+//         $('.menu').animate({ //выбираем класс menu и метод animate
+//
+//             right: '-285px' /* при клике на крестик меню вернется назад в свое
+//              положение и скроется */
+//
+//         }, 200); //скорость движения меню в мс
+//
+//         $('body').animate({ //выбираем тег body и метод animate
+//
+//             right: '0px' //а содержимое страницы снова вернется в положение 0px
+//
+//         }, 200); //скорость движения меню в мс
+//     });
+// };
+
+$(document).ready(main); /* как только страница полностью загрузится, будет
+ вызвана функция main, отвечающая за работу меню *//**
+ * Created by Ника on 04.05.2018.
+ */
